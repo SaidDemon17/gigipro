4ade107fb2c51feef2cb4df986d700556ac0c2ab
SaidDemon17 <andreymeowxd@gmail.com>
2026-04-20 15:38:51 -0500

    pipi

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,15 @@ import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
 
+// Configurar Cloudinary con tus datos
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
